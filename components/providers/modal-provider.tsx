2b5ed6f88db0dd001a13059f5d7cd49343d6870c
Sachin <kumaradity7125@gmail.com>
2023-12-22 08:29:48 +0530

    first commit

---
+++ components/providers/modal-provider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { CreateServerModal } from "../modals/create-server-modal";
+import { InviteModal } from "../modals/invite-modal";
+import { EditServerModal } from "../modals/edit-server-modal";
+import { MemebersModal } from "../modals/members-modal";
+
+export const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  return (
+    <>
+      <CreateServerModal />
+      <InviteModal />
+      <EditServerModal />
+      <MemebersModal />
+    </>
+  );
+};
---
+++ lib/db.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  // In development, use a global instance to avoid multiple instances
+  if (!(globalThis as any).prisma) {
+    (globalThis as any).prisma = new PrismaClient();
+  }
+  prisma = (globalThis as any).prisma;
+}
+
+const db = prisma;
+
+export { db };
